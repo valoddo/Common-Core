5a55a74f2b50b2e66c1e8cc03c5babc48454c282
valoddo <161822352+valoddo@users.noreply.github.com>
2025-03-05 12:53:26 +0100

    Update push_swap.c

--- Push_Swap/push_swap.c
+++ Push_Swap/push_swap.c
@@ -12,7 +12,7 @@
 
 #include "push_swap.h"
 
-void	test(char **c)
+/*void	test_argv2(char **c)
 {
 	int	i;
 	int	j;
@@ -24,17 +24,32 @@ void	test(char **c)
 		printf("%s\n", c[i]);
 		i++;
 	}
+}*/
+void test_fill_a (t_stack *a) 
+{
+	    printf("Stack after filling:\n");
+        while (a) // Stampa lo stack per il controllo 
+		{
+			printf("%d\n", a->value);
+			a = a->next;
+		}
 }
+
 
 int	main(int argc, char **argv)
 {
 	t_stack	*a;
 	t_stack	*b;
 
+	a = NULL;
+	b = NULL;
+
 	if (argc == 1 || (argc == 2 && argv[1][0] == '\0'))
 		ft_error(2);
 	else if (argc == 2)
 		argv = ft_split(argv[0], argv[1], ' ');
-	test(argv);
+	//test_argv2(argv);
+	ft_fill_a(&a, argv);
+	test_fill_a(a);
 	return (0);
 }

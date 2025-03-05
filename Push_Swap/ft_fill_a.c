9e043e99b5daf470352355b5c59bd72250b76c04
valoddo <161822352+valoddo@users.noreply.github.com>
2025-03-05 12:52:05 +0100

    Update ft_fill_a.c

--- Push_Swap/ft_fill_a.c
+++ Push_Swap/ft_fill_a.c
@@ -21,7 +21,6 @@ int ft_atoi(char *argv)
     i = 0;
     nb = 0;
     sign = 1;
-
     while (argv[i])
     {
         if (argv[0] == '+')
@@ -31,66 +30,87 @@ int ft_atoi(char *argv)
             sign = -1;
             i++;
         }
-    if (argv[i] < '0' || argv[i] > '9') // controlla se il primo carattere è un numero
-        ft_error(3);
-    while (argv[i] >= '0' && argv[i] <= '9')
-    {
-        nb = (nb * 10) + (argv[i] - '0');
-        i++;
-    }
-    nb *= sign;
-    return (nb);
+		if (argv[i] < '0' || argv[i] > '9') // controlla se il primo carattere è un numero
+			ft_error(3);
+		while (argv[i] >= '0' && argv[i] <= '9')
+		{
+			nb = (nb * 10) + (argv[i] - '0');
+			i++;
+		}
+	}
+	return (nb *= sign);
+}
+int	ft_nb_is_present(t_stack *a, int nb)
+{
+	while (a)
+	{
+		if (a->value == nb)// Se il numero è già presente
+			return 1; // Restituiamo 1 (vero)
+		a = a->next; // Passiamo al nodo successivo
+	}
+	return 0; // Se il numero non è presente, restituiamo 0 (falso)
+}
+t_stack	*ft_new_node(int nb)
+{
+	t_stack *new_node;
+
+	new_node = (t_stack *)malloc(sizeof(t_stack));
+	if (!new_node)
+		return (NULL); // Restituiamo NULL in caso di errore
+	new_node->value = nb; // Assegniamo il valore al nodo
+	new_node->next = NULL; // Impostiamo il prossimo nodo come NULL (per ora)
+	return (new_node);
 }
 
-int is_duplicate(t_stack *a, int value) // Funzione per controllare se un numero è già nella lista
+void	ft_insert_node (t_stack **a, t_stack *temp)
 {
-    while (stack)
-    {
-        if (stack->value == value)
-            return (1); // Numero duplicato trovato
-        stack = stack->next;
-    }
-    return (0);
-}
-
-
-void add_to_list(t_stack **stack, int value) // Funzione per aggiungere un nodo in coda alla lista
-{
-    t_stack *new = new_node(value);
-    t_stack *temp = *stack;
-
-    if (!new)
-        return;
-    if (!*stack)
-        *stack = new;
-    else
-    {
-        while (temp->next)
-            temp = temp->next;
-        temp->next = new;
+    if (*a == NULL) {
+        *a = temp; // Se lo stack è vuoto, il nuovo nodo diventa la testa
+    } else {
+        temp->next = *a; // Colleghiamo il nuovo nodo alla testa attuale
+        *a = temp; // Il nuovo nodo diventa la nuova testa
     }
 }
 
-
-int ft_fill_a(t_stack **a, char **argv)
+void	ft_fill_a(t_stack **a, char **argv)
 {
-    int j;
-    int nb;
-    t_stack *temp;
+	int	j;
+	int	nb;
+	t_stack	*temp;
 
-    j = 1; // Iniziamo dal secondo argomento (argv[1])
-    while (argv[j]) // ciclo per ogni argomento
-    {
-        nb = ft_atoi(argv[j]); // Usa la funzione ft_atoi per convertire la stringa in un numero
-        temp = *a;
-        while (temp) // Controllo duplicati
-        {
-            if (temp->value == nb) // Se il numero è già presente nella lista
-                return (4); // Errore per numero duplicato
-            temp = temp->next;
-        }
-        add_to_list(a, nb); // Aggiungi il numero alla lista
-        j++; // Passa al prossimo argomento
+	j = 1; // Iniziamo dal secondo argomento (argv[1])
+	while (argv[j]) // ciclo per ogni argomento
+	{
+		nb = ft_atoi(argv[j]); // Usa la funzione ft_atoi per convertire la stringa in un numero
+		if (ft_nb_is_present(*a, nb) == 1) 
+			ft_error(4);
+        temp = ft_new_node(nb); // Creiamo il nuovo nodo con il numero
+        ft_insert_node(a, temp); // Inseriamo il nodo nello stack
+        j++; // Passiamo al prossimo argomento
     }
-    return (0); // Successo
+}
+
+/*int main(int argc, char **argv) 
+{
+	t_stack *a = NULL; // Inizializziamo lo stack come NULL
+    if (argc < 2) 
+	{
+        printf("Usage: ./program <numbers>\n");
+        return 1;
+    }
+
+    // Riempie lo stack con i numeri da argv
+    if (ft_fill_a(&a, argv) == 1) 
+	{
+        printf("Stack after filling:\n");
+        while (a) // Stampa lo stack per il controllo 
+		{
+			printf("%d\n", a->value);
+			a = a->next;
+		}
+    } else {
+        printf("Error while filling stack\n");
+        return 1;
+    }
+    return 0;
 }

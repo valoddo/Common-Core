0c379bd1442fb4245fb9b268d1db62a963f8c870
valoddo <161822352+valoddo@users.noreply.github.com>
2025-03-06 13:31:28 +0100

    Update ft_itoa.c

--- Prove di esame Rank 02/Level 4/ft_itoa.c
+++ Prove di esame Rank 02/Level 4/ft_itoa.c
@@ -10,17 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-static char	*ft_char(char *s, unsigned int number, long int len)
-{
-	while (number > 0)
-	{
-		s[len--] = 48 + (number % 10);
-		number = number / 10;
-	}
-	return (s);
-}
+#include <stdlib.h>
+#include <stdio.h>
 
 static long int	ft_len(int n)
 {
@@ -37,31 +28,31 @@ static long int	ft_len(int n)
 	return (len);
 }
 
-char	*ft_itoa(int n)
+char	*ft_itoa(int nbr)
 {
-	char				*s;
-	long int			len;
-	unsigned int		number;
-	int					sign;
+	int		len;
+	char	*str;
+	long	n;
 
-	sign = 1;
+	n = nbr;
 	len = ft_len(n);
-	s = (char *)malloc(sizeof(char) * (len + 1));
-	if (!(s))
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
 		return (NULL);
-	s[len--] = '\0';
-	if (n == 0)
-		s[0] = '0';
+	str[len] = '\0';
 	if (n < 0)
 	{
-		sign *= -1;
-		number = n * -1;
-		s[0] = '-';
+		str[0] = '-';
+		n = -n;
 	}
-	else
-		number = n;
-	s = ft_char(s, number, len);
-	return (s);
+	while (len--)
+	{
+		if (str[len] == '-')
+			break;
+		str[len] = (n % 10) + '0';
+		n /= 10;
+	}
+	return (str);
 }
 
 // int main() 

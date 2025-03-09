a92a56142fa11ba4ceeff073d3ac4ee6b18cc825
valoddo <161822352+valoddo@users.noreply.github.com>
2025-03-09 22:21:12 +0100

    Update pipex.c

--- Pipex/Pipex Final/pipex.c
+++ Pipex/Pipex Final/pipex.c
@@ -42,10 +42,14 @@ static char	*check_args(char **paths, char *argcmd)
 }
 static void first_child_process(t_pipex pipex, char **argv, char **envp)
 {
-    dup2(pipex.pipe_fd[1], STDOUT_FILENO);
- 	dup2(pipex.infd, 0);
+    pipex.infd = open(argv[1], O_RDONLY);
+	if (pipex.infd == -1)
+		ft_error("zsh", 1);
+	dup2(pipex.pipe_fd[1], STDOUT_FILENO);
+ 	dup2(pipex.infd, STDIN_FILENO);
  	close(pipex.pipe_fd[1]);
  	close(pipex.infd);
+	close(pipex.outfd);
  	pipex.arg_cmd = ft_split(argv[2], ' ');
  	if (!pipex.arg_cmd || !pipex.arg_cmd[0])
  		ft_error("Cmd", 3);
@@ -58,48 +62,47 @@ static void first_child_process(t_pipex pipex, char **argv, char **envp)
  }
  
  static void	second_child_process(t_pipex pipex, char **argv, char **envp)
- {
-	free_cmd_and_args(pipex.arg_cmd, pipex.cmd);
- 	dup2(pipex.pipe_fd[0], STDIN_FILENO);
- 	dup2(pipex.outfd, 1);
+{
+	pipex.outfd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (pipex.outfd == -1)
+		ft_error("zsh", 1);
+	dup2(pipex.pipe_fd[0], STDIN_FILENO);
+ 	dup2(pipex.outfd, STDOUT_FILENO);
  	close(pipex.pipe_fd[0]);
  	close(pipex.outfd);
- 	pipex.arg_cmd = ft_split(argv[3], ' ');
- 	if (!pipex.arg_cmd || !pipex.arg_cmd[0])
+	close(pipex.infd);
+ 	pipex.arg_cmd2 = ft_split(argv[3], ' ');
+ 	if (!pipex.arg_cmd2 || !pipex.arg_cmd2[0])
  		ft_error("Cmd", 3);
- 	pipex.cmd = check_args(pipex.paths, pipex.arg_cmd[0]);
- 	if (!pipex.cmd)
- 		ft_error("Cmd", 3);
- 	execve(pipex.cmd, pipex.arg_cmd, envp);
- 	if (execve(pipex.cmd, pipex.arg_cmd, envp) == -1)
- 		ft_error("zsh", 1);
+ 	pipex.cmd2 = check_args(pipex.paths, pipex.arg_cmd2[0]);
+ 	if (!pipex.cmd2)
+		ft_error("Cmd", 3);
+	execve(pipex.cmd2, pipex.arg_cmd2, envp);
+	if (execve(pipex.cmd2, pipex.arg_cmd2, envp) == -1)
+		ft_error("zsh", 1);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	t_pipex	pipex;
-
+	
 	if (argc != 5)
-		ft_error("Main", 2);
-	pipex.paths = ft_get_path(envp);
-	pipex.infd = open(argv[1], O_RDONLY);
-	pipex.outfd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (pipex.infd == -1 || pipex.outfd == -1)
-		ft_error("zsh", 1);
-	if (pipe(pipex.pipe_fd) == -1)
-		ft_error("zsh", 1);
-	pipex.p_id = fork();
-	if (pipex.p_id == 0)
-		first_child_process(pipex, argv, envp);
-	close(pipex.pipe_fd[1]);
-	pipex.p_id_2 = fork();
-	if (pipex.p_id_2 == 0)
+	ft_error("Main", 2);
+pipex.paths = ft_get_path(envp);
+if (pipe(pipex.pipe_fd) == -1)
+	ft_error("zsh", 1);
+pipex.p_id = fork();
+if (pipex.p_id == 0)
+	first_child_process(pipex, argv, envp);
+close(pipex.pipe_fd[1]);
+pipex.p_id_2 = fork();
+if (pipex.p_id_2 == 0)
 	second_child_process(pipex, argv, envp);
-	waitpid(pipex.p_id, NULL, 0);
-	waitpid(pipex.p_id_2, NULL, 0);
-	free_cmd_and_args(pipex.arg_cmd, pipex.cmd);
-	close(pipex.pipe_fd[0]);
-	close(pipex.infd);
-	close(pipex.outfd);
-	return (0);
+close(pipex.pipe_fd[0]);
+waitpid(pipex.p_id, NULL, 0);
+waitpid(pipex.p_id_2, NULL, 0);
+ft_free_split(pipex.paths);
+close(STDOUT_FILENO);
+close(STDIN_FILENO);
+return (0);
 }

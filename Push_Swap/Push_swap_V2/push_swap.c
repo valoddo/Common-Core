db970b8d6eb90b9d4f9f42af878a92eae6ffb921
valoddo <161822352+valoddo@users.noreply.github.com>
2025-03-20 15:08:42 +0100

    Update push_swap.c

--- Push_Swap/Push_swap_V2/push_swap.c
+++ Push_Swap/Push_swap_V2/push_swap.c
@@ -106,6 +106,143 @@ void	calculate_push_cost(t_stack *a, t_stack *b)
 	final_rotate(a); // Allinea lo stack A
 }*/
 
+// Prototipi delle funzioni aggiuntive
+void		push_chunks(t_stack **a, t_stack **b, int chunk_start, int chunk_end);
+void		sort_chunk(t_stack **a, t_stack **b, int chunk_size);
+void		greedy_reinsert(t_stack **a, t_stack **b);
+void		final_align(t_stack **a);
+
+// Funzione principale modificata
+void	push_swap(t_stack **a, t_stack **b)
+{
+	int	chunk;
+	int	chunk_size;
+
+	chunk_size = 20;
+	if (ft_stacklen(*a) <= 5)
+	{
+		sort_small(a, b);
+		return ;
+	}
+	chunk = 4; // Fase 1: Push dei chunk in ordine inverso (80-99, 60-79, ..., 0-19)
+	while (chunk >= 0)
+	{
+		push_chunks(a, b, chunk * chunk_size, (chunk * chunk_size) + (chunk_size - 1));
+		chunk--;
+	}
+	chunk = 0; // Fase 2: Ordina e reinserisci i chunk
+	while (chunk < 5)
+	{
+		sort_chunk(a, b, chunk_size);
+		greedy_reinsert(a, b);
+		chunk++;
+	}
+	final_align(a); // Fase 3: Allineamento finale
+}
+
+// Implementazione nuove funzioni
+void	push_chunks(t_stack **a, t_stack **b, int chunk_start, int chunk_end)
+{
+	int		i;
+	int		count;
+	int		stack_len;
+
+	i = 0;
+	count = 0;
+	stack_len = ft_stacklen(*a);
+	while (i < stack_len && count < 20)
+	{
+		if ((*a)->index >= chunk_start && (*a)->index <= chunk_end)
+		{
+			ft_push_b(a, b);
+			if ((*b)->index > chunk_start + (chunk_end - chunk_start) / 2)
+				ft_rotate_b(b);
+			count++;
+		}
+		else
+			ft_rotate_a(a);
+		i++;
+	}
+}
+
+void	sort_chunk(t_stack **a, t_stack **b, int chunk_size)
+{
+	int	bit;
+	int	i;
+
+	bit = 0;
+	while (bit < 5)
+	{
+		i = 0;
+		while (i < chunk_size)
+		{
+			if (((*b)->index >> bit) & 1)
+				ft_rotate_b(b);
+			else
+				ft_push_a(a, b);
+			i++;
+		}
+		while (ft_stacklen(*a) > 100 - chunk_size)
+			ft_push_b(a, b);
+		bit++;
+	}
+}
+
+void	greedy_reinsert(t_stack **a, t_stack **b)
+{
+	t_stack	*cheapest;
+	int		cost_a;
+	int		cost_b;
+
+	while (*b)
+	{
+		init_nodes(*a, *b);
+		cheapest = find_cheapest(*b);
+		cost_a = cheapest->target->current_position;
+		cost_b = cheapest->current_position;
+		while (cost_a > 0 && cost_b > 0 && 
+				cheapest->target->above_median && cheapest->above_median) // Esegui rotazioni ottimizzate
+		{
+			ft_rotate_r(a, b);
+			cost_a--;
+			cost_b--;
+		}
+		while (cost_a < 0 && cost_b < 0 && 
+				!cheapest->target->above_median && !cheapest->above_median)
+		{
+			ft_reverse_rotate_r(a, b);
+			cost_a++;
+			cost_b++;
+		}
+		rotate_a_to_position(a, cost_a);
+		rotate_b_to_position(b, cost_b);
+		ft_push_a(a, b);
+	}
+}
+
+void	final_align(t_stack **a)
+{
+	t_stack	*min_node;
+	int		min_pos;
+	int		stack_len;
+
+	min_node = find_min_node(*a);
+	min_pos = min_node->current_position;
+	stack_len = ft_stacklen(*a);
+	
+	if (min_pos > stack_len / 2)
+	{
+		min_pos = stack_len - min_pos;
+		while (min_pos--)
+			ft_reverse_rotate_a(a);
+	}
+	else
+	{
+		while (min_pos--)
+			ft_rotate_a(a);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_stack	*a;
@@ -150,3 +287,4 @@ int	main(int argc, char **argv)
 	test_fill_a (a);
 	//ft_error(&a, "Test memory free", 1);
 	//ft_fill_stack(&a, argv);*/
+

c15c0aa9b175c7d89588bdc97f3573adb2b2c1d7
valoddo <161822352+valoddo@users.noreply.github.com>
2025-03-24 08:15:08 +0100

    Update algorithm.c

--- Push_Swap/Push_Swap_V3/algorithm.c
+++ Push_Swap/Push_Swap_V3/algorithm.c
@@ -113,6 +113,58 @@ void	ft_move_node(t_stack **a, t_stack **b)
 	ft_push_a(a, b);
 }
 
+/*void	ft_move_node(t_stack **a, t_stack **b)
+{
+	t_stack	*cheapest_node;
+	int		a_direction;
+	int		b_direction;
+	int		a_moves;
+	int		b_moves;
+
+	cheapest_node = ft_cheapest(*b);
+	a_direction = 1;
+	if (cheapest_node->target_node->index > ft_stacklen(*a) / 2)
+		a_direction = 0;
+	b_direction = 1;
+	if (cheapest_node->index > ft_stacklen(*b) / 2)
+		b_direction = 0;
+	a_moves = cheapest_node->target_node->index;
+	if (a_direction == 0)
+		a_moves = ft_stacklen(*a) - cheapest_node->target_node->index;
+	b_moves = cheapest_node->index;
+	if (b_direction == 0)
+		b_moves = ft_stacklen(*b) - cheapest_node->index;
+	while (a_moves > 0 || b_moves > 0)
+	{
+		if (a_direction == b_direction && a_moves > 0 && b_moves > 0)
+		{
+			if (a_direction == 1)
+				ft_rotate_ab(a, b);
+			else
+				ft_reverse_ab(a, b);
+			a_moves--;
+			b_moves--;
+		}
+		else if (a_moves > 0)
+		{
+			if (a_direction == 1)
+				ft_rotate_a(a);
+			else
+				ft_reverse_a(a);
+			a_moves--;
+		}
+		else if (b_moves > 0)
+		{
+			if (b_direction == 1)
+				ft_rotate_b(b);
+			else
+				ft_reverse_b(b);
+			b_moves--;
+		}
+  	ft_push_a(a, b);
+	}
+}*/
+
 t_stack	*ft_cheapest(t_stack *b)
 {
 	t_stack	*cheapest;

3b3ed20163d8dec4a7119127763a0876455d2c27
valoddo <161822352+valoddo@users.noreply.github.com>
2025-03-25 18:34:43 +0100

    Add files via upload

---
+++ Push_Swap/Push_Swap_Final/push_swap.h
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vloddo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/28 14:21:10 by vloddo            #+#    #+#             */
+/*   Updated: 2025/02/28 14:21:11 by vloddo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdbool.h>
+# include <limits.h>
+
+typedef struct s_stack
+{
+	int					value;
+	int					index;
+	int					current_position;
+	int					push_price;
+	bool				above_median;
+	struct s_stack		*target_node;
+	struct s_stack		*next;
+}				t_stack;
+
+// ft_split.c
+char	**ft_split(char const *s0, char const *s1, char c);
+
+// ft_fill_stack.c
+void	ft_fill_stack(t_stack **a, char **argv, int argc);
+
+// index_bubble.c
+void	ft_set_index(t_stack *a);
+
+// push_swap_utils.c
+int		ft_stacklen(t_stack *a);
+void	ft_error(t_stack **a, char *msg, char **argv, int argc);
+void	ft_putstr(char *str);
+void	ft_free_stack(t_stack **stack);
+void	ft_free_argv(char **argv);
+
+// sort_small_stack.c
+void	ft_sort_small_stack(t_stack **a, t_stack **b);
+void	sort_three(t_stack **a);
+void	ft_better_move(int pos, t_stack **a);
+
+// algorithm.c
+void	ft_target_node(t_stack *a, t_stack *b);
+t_stack	*ft_min_node(t_stack *a);
+void	ft_move_node(t_stack **a, t_stack **b);
+t_stack	*ft_cheapest(t_stack *b);
+void	finish_rotation(t_stack **stack, t_stack *node, char stack_name);
+
+// swap_command.c
+void	ft_swap_a(t_stack **a);
+void	ft_swap_b(t_stack **b);
+void	ft_swap_ab(t_stack **a, t_stack **b);
+
+// rotate_command.c
+void	ft_rotate_a(t_stack **a);
+void	ft_rotate_b(t_stack **b);
+void	ft_rotate_ab(t_stack **a, t_stack **b);
+
+//reverse_command.c
+void	ft_reverse_a(t_stack **a);
+void	ft_reverse_b(t_stack **b);
+void	ft_reverse_ab(t_stack **a, t_stack **b);
+
+//push_command.c
+void	ft_push_a(t_stack **a, t_stack **b);
+void	ft_push_b(t_stack **a, t_stack **b);
+
+// ft_calloc.c
+void	*ft_calloc(size_t num, size_t size);
+
+//push_swap.c
+void	ft_current_position(t_stack *stack);
+void	ft_set_price(t_stack *a, t_stack *b);
+
+#endif
---
+++ Push_Swap/Push_Swap_Final/rotate_command.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vloddo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/28 14:20:52 by vloddo            #+#    #+#             */
+/*   Updated: 2025/02/28 14:20:54 by vloddo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	rotate(t_stack **head)
+{
+	t_stack	*first;
+	t_stack	*last;
+
+	if (!head || !*head || !(*head)->next)
+		return ;
+	first = *head;
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+	*head = first->next;
+	first->next = NULL;
+	last->next = first;
+}
+
+void	ft_rotate_a(t_stack **a)
+{
+	rotate(a);
+	write(1, "ra\n", 3);
+}
+
+void	ft_rotate_b(t_stack **b)
+{
+	rotate(b);
+	write(1, "rb\n", 3);
+}
+
+void	ft_rotate_ab(t_stack **a, t_stack **b)
+{
+	rotate(a);
+	rotate(b);
+	write(1, "rr\n", 3);
+}

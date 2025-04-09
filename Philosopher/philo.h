865858506ba52da5f7ec0f4f2a2ff81e42b6475f
valoddo <161822352+valoddo@users.noreply.github.com>
2025-04-09 19:16:41 +0200

    Add files via upload

---
+++ Philosopher/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vloddo <vloddo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 17:49:35 by vloddo            #+#    #+#             */
+/*   Updated: 2025/04/09 18:12:09 by vloddo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <pthread.h> //libreria dei thread
+# include "ft_printf/ft_printf.h"
+
+# endif
+
